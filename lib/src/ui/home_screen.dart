5a789245407a33024ecd6c8d2254045a37f9c8ce
Sergey <kazbers@gmail.com>
2021-08-24 13:34:33 +0300

    added Movie model

--- lib/src/ui/home_screen.dart
+++ lib/src/ui/home_screen.dart
@@ -3,6 +3,34 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(
+          Icons.menu,
+          color: Colors.black45,
+        ),
+        title: Text(
+          'Movies-db'.toUpperCase(),
+          style: Theme.of(context).textTheme.caption!.copyWith(
+                color: Colors.black45,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'muli',
+              ),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(
+              right: 15,
+            ),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/images/logo.jpg'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
